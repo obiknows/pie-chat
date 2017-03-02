b707a39ff295c285fda21ea8e1318844be1a9c25
samnnodim <sam.nnodim@gmail.com>
2017-03-01 21:03:06 -0500

    added initial framework

---
+++ UdpChat.py
@@ -0,0 +1,15 @@
+# UdpChat.py -> a UDP Chat application in python
+# written by sam nnodim (son2105)
+
+import sys
+
+
+# MAIN LOOP
+def main(argv):
+    # parse the commmandline args
+
+    print (argv)
+
+# run the program
+if __name__ == '__main__':
+    main(sys.argv)
